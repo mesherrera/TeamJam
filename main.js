3f223eedeb71f7dced311a7a3b39ed624bf5c76f
Jackie Lin <jackie.lin128@gmail.com>
2019-08-01 13:54:54 -0400

    jackie changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let translateTarget;
 let word;
 let partOfSpeech;
+let wikiLink;
+let googleTransLink;
 
 const btn = document.getElementById("myCheck");
 // On mouse-over, execute myFunction
@@ -20,11 +22,15 @@ function myFunction() {
       // console.log(data);
       word = data[0].shortdef;
       partOfSpeech = data[0].fl;
+      wikiLink = `https://es.wikipedia.org/wiki/${word}`;
+      googleTransLink = `https://translate.google.com/#view=home&op=translate&sl=en&tl=es&text=${translateTarget}`
       createDictionaryRef();
       // console.log("word", word, "pOS", partOfSpeech);
       // document.body.style.backgroundColor = "red"; //DEBUG, Remove Me Later
     })
     .catch(() => console.log("error"));
+
+
 }
 
 if (btn) {
@@ -39,19 +45,46 @@ function realCreateDictionaryRef() {
 
   function callback() {
     if(count > 0) {
+      //create Divs for fetch items
       const outputPOS = document.createElement("div");
       const outputDef = document.createElement("div");
       const outputOriginal = document.createElement("div");
-
+      //create buttons for google and wiki
+      const wikiBtn = document.createElement("button");
+      const googleBtn = document.createElement("button");
+      const wikipLink = document.createElement("a");
+      const googleLink = document.createElement("a");
+      
+      // event.preventDefault(wikiBtn);
+      // event.preventDefault(googleBtn);
+      
       //Assigns inner text to each created div
       outputDef.innerText = word;
       outputPOS.innerText = partOfSpeech;
       outputOriginal.innerText = translateTarget;
-
+      
+      //give those buttons Ids
+      wikiBtn.setAttribute('id','wikiBtn');
+      googleBtn.setAttribute('id','googBtn');
+      //add links to a tags
+      wikipLink.setAttribute('href',wikiLink);
+      googleLink.setAttribute('href',googleTransLink)
+      wikipLink.innerHTML = 'Wikipedia';
+      googleLink.innerHTML = 'Google Translate'
+      
+      
       // Add to pop up
       document.querySelector("#dictionary").appendChild(outputOriginal);
       document.querySelector("#dictionary").appendChild(outputDef);
       document.querySelector("#dictionary").appendChild(outputPOS);
+      //add Buttons to pop up
+      document.querySelector("#dictionary").appendChild(wikiBtn);
+      document.querySelector("#dictionary").appendChild(googleBtn);
+      //Add links to goog and wiki buttons
+      document.querySelector('#wikiBtn').appendChild(wikipLink);
+      document.querySelector('#googBtn').appendChild(googleLink);
+
+      
     }
     count++;
   }
