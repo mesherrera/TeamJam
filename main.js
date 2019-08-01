3501bcbc8644de285f4ac35f41b9efe74ecb802e
Michelle Herrera <mesherrera@aol.com>
2019-08-01 02:29:40 -0400

    Logo pics

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-const translateTarget = target;
-var word;
-var partOfSpeech;
+let translateTarget;
+let word;
+let partOfSpeech;
 
-fetch(
-  `https://www.dictionaryapi.com/api/v3/references/spanish/json/${translateTarget}?key=51a3d131-7eb1-4a9a-926b-29d55b25e4c1`
-)
-  .then(data => data.json())
-  .then(data => {
-    word = data[0].shortdef;
-    partOfSpeech = data[0].fl;
-  });
+const btn = document.getElementById("myCheck");
+// On mouse-over, execute myFunction
+function myFunction() {
+  event.preventDefault(translateTarget);
+  translateTarget = window.getSelection().toString();
+  console.log(translateTarget);
+  fetch(
+    `https://www.dictionaryapi.com/api/v3/references/spanish/json/${translateTarget}?key=51a3d131-7eb1-4a9a-926b-29d55b25e4c1`
+  )
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      word = data[0].shortdef;
+      partOfSpeech = data[0].fl;
+      console.log("word", word, "pOS", partOfSpeech);
+      createDictionaryRef();
+      document.body.style.backgroundColor = "red"; //DEBUG, Remove Me Later
+    })
+    .catch(() => console.log("error"));
+}
+
+if (btn) {
+  btn.addEventListener("click", myFunction);
+  /// Fetch request
+}
 
 // Create divs for the pop up
-let pOS = document.createElement("div");
-let def = document.createElement("div");
+function createDictionaryRef() {
+  const outputPOS = document.createElement("div");
+  const outputDef = document.createElement("div");
 
-//Assigns inner text to each created div
-spanishWord.innerHTML(word);
-def.innerHTML(definition);
-pOS.innerHTML(partOfSpeech);
+  //Assigns inner text to each created div
+  outputDef.innerText = word;
+  outputPOS.innerText = partOfSpeech;
+
+  // Add to pop up
+  document.querySelector("#dictionary").appendChild(outputDef);
+  document.querySelector("#dictionary").appendChild(outputPOS);
+}
