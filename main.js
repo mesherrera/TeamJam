92e65c3599b6ee9bd7913d6a41f0611dc82c584a
Andy Wong <andywong.ny@gmail.com>
2019-08-01 04:36:51 -0400

    lolidk

--- main.js
+++ main.js
@@ -6,19 +6,23 @@ const btn = document.getElementById("myCheck");
 // On mouse-over, execute myFunction
 function myFunction() {
   event.preventDefault(translateTarget);
-  translateTarget = window.getSelection().toString();
-  // console.log(translateTarget);
+  chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+    translateTarget = selection;
+    // alert(selection[0]);
+  });
   fetch(
     `https://www.dictionaryapi.com/api/v3/references/spanish/json/${translateTarget}?key=51a3d131-7eb1-4a9a-926b-29d55b25e4c1`
   )
     .then(data => data.json())
     .then(data => {
-      console.log(data);
+      // console.log(data);
       word = data[0].shortdef;
       partOfSpeech = data[0].fl;
-      console.log("word", word, "pOS", partOfSpeech);
       createDictionaryRef();
-      document.body.style.backgroundColor = "red"; //DEBUG, Remove Me Later
+      // console.log("word", word, "pOS", partOfSpeech);
+      // document.body.style.backgroundColor = "red"; //DEBUG, Remove Me Later
     })
     .catch(() => console.log("error"));
 }
@@ -29,15 +33,25 @@ if (btn) {
 }
 
 // Create divs for the pop up
-function createDictionaryRef() {
-  const outputPOS = document.createElement("div");
-  const outputDef = document.createElement("div");
+function realCreateDictionaryRef() {
+  let count = 0;
+  function callback() {
+    if(count > 0) {
+      const outputPOS = document.createElement("div");
+      const outputDef = document.createElement("div");
 
-  //Assigns inner text to each created div
-  outputDef.innerText = word;
-  outputPOS.innerText = partOfSpeech;
+      //Assigns inner text to each created div
+      outputDef.innerText = word;
+      outputPOS.innerText = partOfSpeech;
 
-  // Add to pop up
-  document.querySelector("#dictionary").appendChild(outputDef);
-  document.querySelector("#dictionary").appendChild(outputPOS);
+      // Add to pop up
+      document.querySelector("#dictionary").appendChild(outputDef);
+      document.querySelector("#dictionary").appendChild(outputPOS);
+    }
+    count++;
+  }
+  return callback;
 }
+
+const createDictionaryRef = realCreateDictionaryRef();
+
